12a644db84497b561643c947e1cfd0dd2a6ea401
Gleisson Bezerra <gleisson.bezerra@hotmail.com>
2025-01-31 06:59:39 -0300

    AULA 03

--- AULA03/exercicios/2.PID/controller/controller.ino
+++ AULA03/exercicios/2.PID/controller/controller.ino
@@ -1,184 +1,124 @@
-#include "Wire.h"
 #include "Servo.h"
+#include "Wire.h"
 
-#include "DHT.h"
- 
-#define DHTPIN A1 // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT11 
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define SERVO_PIN 6 // Digital pin connected to the DHT sensor
 
-#define SAMPLE_TIME 100 // PID interval in miliseconds
+#define SAMPLE_TIME 200 // PID interval in miliseconds
+
+#define SET_POINT 50 //
+#define START_TEMPERATURE 30
 
 #define VALVE_OFFSET 90
-
-#define SLAVE_ADDRESS 0x08 //SLAVE Unique ID in the BUS
- 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
-// Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-DHT dht(DHTPIN, DHTTYPE);
-
-String inputString = "";//Store i2c data
-String outputString = "";//enqueue i2c data
-boolean stringComplete = false;  // whether the string is complete
-boolean halt = false;
+#define WARMUP_RATE 0.05
+#define COOLDOWN_RATE 0.01
 
 Servo valveServo; // Servo controller
+
 int currentPos = 0; // Servo position 0 = midpoint
-int avgCount = 0;
 
-float tSetPoint;
+float kp = 5, ki = 2.5, kd = 0.01;
 
-float kp = 10, ki = 5, kd = 0.1;
-
-long time;
+long time, startTime;
 long et;
 
-long pTime, lTime, rTime, sampleTime, reportTime;
-
-float dt, error, pError;
-
-float integralError = 0;
-float derivativeError = 0;
-
-float h;
-float t, avgTemp = 0;
+float dt, error = 0, pError = 0, integralError = 0, derivativeError = 0;
+float currentTemp = START_TEMPERATURE, pid;
 
 
-void receiveData(int bytecount)
-{
-  char inChar;
-
-  for (int i = 0; i < bytecount; i++) {
-    inChar = Wire.read();
-    inputString += String(inChar);
-  }
-
-  stringComplete = true;
-
-}
-void sendData()
-{
-  if ( outputString != "" )
-  {
-    //Serial.println(outputString);
-    Wire.write(outputString.c_str());
-    outputString = "";
-  }
-
-}
- 
 void setup() 
 {
-  Serial.begin(9600);
-  while(!Serial);
-  Serial.println("SP, PV, CO");
-
-  dht.begin();
-
-  Wire.begin(SLAVE_ADDRESS);
-
-  Wire.onReceive(receiveData);
-  Wire.onRequest(sendData);
-
-  valveServo.attach(SERVO_PIN);
-  valveServo.write(VALVE_OFFSET + currentPos); 
-
-  tSetPoint = dht.readTemperature();
-
   pinMode(LED_BUILTIN, OUTPUT);
 
-  for( int i = 0; i < 3; ++i)
-  {
-    tSetPoint = max(tSetPoint, dht.readTemperature());
-    
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(1000);                       // wait for a second
+  Serial.begin(9600);
+  Serial.println("SP, PV");
+
+  valveServo.attach(SERVO_PIN);
+  valveServo.write(VALVE_OFFSET + currentPos);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
   }
 
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.cp437(true);  // Use full 256 char 'Code Page 437' font
+
+  // Clear the buffer
+  display.clearDisplay();
+
+
+  for ( int i=0; i < 3; ++i)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+
+  }
+
+  startTime = millis();
+  randomSeed(startTime);
 }
  
+
 void loop() 
 {
-
-  if ( stringComplete )
-  {
-    //Serial.println(inputString);
-    if ( inputString == "close")
-    {
-      halt = true;
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      //Serial.println("Critical stop due to alarm condition. Please, reset manually!");
-      valveServo.write(0);
-    }
-    
-    stringComplete = false;
-    inputString = "";  
-  }
   
-  // Reading temperature or humidity takes about 250 milliseconds!
-  h = dht.readHumidity();
-  t = dht.readTemperature();
+  et = millis() - startTime;
 
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(t) || isnan(h)) 
+  if ( et > SAMPLE_TIME)
   {
-    //Serial.println("Failed to read from DHT");
-  } 
-  else
-  {
+    startTime = millis();
 
-    et = millis() - sampleTime;
+    Serial.print(SET_POINT);
+    Serial.print(" ");
+    Serial.println(currentTemp);
 
-    if ( et > SAMPLE_TIME && avgCount > 0)
-    {
-      avgTemp = (float)avgTemp/avgCount;
+    dt = (float)et/1000;
+    error = SET_POINT - currentTemp;
 
-     Serial.print(tSetPoint);
-     Serial.print(" ");
-     Serial.print(avgTemp);
+    integralError = integralError + (float)error * dt;
+    derivativeError = (float)(error - pError)/dt;
 
-      sampleTime = millis();
-      dt = (float)et/1000;
+    pError = error;
 
-      error = tSetPoint - avgTemp;
+    pid = kp*error + ki * integralError + kd * derivativeError;
 
-      integralError = integralError + (float)error * dt;
-      derivativeError = (float)(error - pError)/dt;
+    currentPos = VALVE_OFFSET - (int)(pid);
+    currentPos = constrain(currentPos,0,180);
 
-      pError = error;
+    valveServo.write(currentPos);
 
-      currentPos = VALVE_OFFSET + (int)(kp*error + ki * integralError + kd * derivativeError);
-      currentPos = constrain(currentPos,0,180); 
+    currentTemp += (pid * WARMUP_RATE) - (currentTemp - START_TEMPERATURE) * COOLDOWN_RATE;
 
-      if (!halt)
-      {
-        valveServo.write(currentPos);
-      }else{
-        currentPos = 0;
-      }
-        
-     Serial.print(" ");
-     Serial.println(currentPos);
-
-      avgTemp = 0;
-      avgCount = 0;
-
-    }else{
-      avgTemp += t;
-      avgCount++;
-    }
-
-
-    //prepare i2c message to master
-    outputString = String("{\"t\":"+String(t)+", \"h\":"+String(h)+"}");
-    
   }
+
+  if ( random(100) > 85 && (int)(millis()/1000) % 15 == 0 )
+  {
+    currentTemp += random(20);
+    currentTemp -= random(10);
+  }
+
+  display.clearDisplay();
+  display.setCursor(35, 20);
+  display.print(currentTemp);
+  display.display();
+
+  
 }
